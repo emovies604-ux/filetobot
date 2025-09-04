10cb2068a3aaa538a9322846501f157477b2be64
emovies604-ux <emovies604@gmail.com>
2025-09-04 11:24:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,3 +9,6 @@ MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 # Database + Collection names
 DB_NAME = os.getenv("DB_NAME", "filetobot")
 COLLECTION_NAME = os.getenv("COLLECTION_NAME", "files")
+
+# Files channel (must be your channel ID, like -1001234567890)
+FILES_CHANNEL = int(os.getenv("FILES_CHANNEL", "-1002789851054"))
